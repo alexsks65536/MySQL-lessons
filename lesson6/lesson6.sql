574239f60b4a9e817c0ea603614eb4ef236e0b22
alexsks65536 <megbyte@mail.ru>
2021-09-05 23:18:14 +0300

    Урок 6. Вебинар. Операторы, фильтрация, сортировка и ограничение. Агрегация данных

---
+++ lesson6/lesson6.sql
@@ -0,0 +1,58 @@
+/*
+* 1. Пусть задан некоторый пользователь. Из всех пользователей соц. сети найдите человека, 
+* который больше всех общался  с выбранным пользователем (написал ему сообщений).
+*/
+
+SELECT from_user_id, to_user_id, -- вывод пользователя от кого и кому
+(SELECT concat(first_name,' ', last_name) FROM users WHERE id = messages.from_user_id)  AS name, -- находим его имя и фамилию из профиля
+count(*) AS total -- находим кол-во сообщений из messages
+FROM messages 
+WHERE to_user_id = 6 -- задаем искомого пользователя
+GROUP BY from_user_id -- группируем
+ORDER BY total DESC -- сортируем по найденному кол-ву 'total'
+LIMIT 1; -- выводим только одно значение
+
+/*
+ * 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
+ */
+-- возраст пользователей по возрастанию
+SELECT user_id, (YEAR(current_date())-YEAR(birthday)) AS age FROM profiles ORDER BY age;
+
+-- общее кол-во пользователей младше 10 лет
+SELECT COUNT(*) as quantity_over_10 FROM profiles WHERE (YEAR(current_date())-YEAR(birthday)) < '10';
+
+-- id пользователей, которые младше 10 лет
+SELECT user_id AS user_10 FROM profiles WHERE (YEAR(current_date())-YEAR(birthday)) < '10';
+-- 
+SELECT user_id AS user_10, (SELECT like_type FROM posts_likes WHERE profiles.user_id = user_10 ) FROM profiles WHERE (YEAR(current_date())-YEAR(birthday)) < '10';
+
+-- 
+SELECT user_id AS s, (YEAR(current_date())-YEAR(birthday)) AS age, (SELECT like_type FROM posts_likes WHERE user_id = s) FROM profiles ORDER BY age;
+
+/*
+ *3. Определить кто больше поставил лайков (всего): мужчины или женщины.
+ */
+
+-- вывод общего кол-во 'm' и 'f' в таблице 'profiles'
+SELECT gender, COUNT(*) -- выбрать из столбца "gender", общее кол-во из
+	FROM profiles -- из таблицы 'profiles'
+	WHERE gender = 'm' OR gender = 'f' -- только из значений 'm', 'f'
+	GROUP BY gender -- группировать по 'gender'
+	ORDER BY gender ASC; -- сортировать по 'gender'
+	
+-- вывод таблицы лайков, которая соответствует gender
+SELECT user_id, like_type, ( -- выбрать из столбцов user_id, like_type
+	SELECT gender -- выбрать из столбца 'gender'
+	FROM profiles -- таблицы profiles
+	WHERE profiles.user_id = posts_likes.user_id) -- значение user_id таблицы profiles которое соответствует значению user_id в таблице posts_like
+	AS gender -- название столбца gender
+	FROM posts_likes; -- из таблицы posts_like
+	
+
+SELECT like_type, ( -- выбрать из столбцов user_id, like_type
+	SELECT gender -- выбрать из столбца 'gender'
+	FROM profiles -- таблицы profiles
+	WHERE profiles.user_id = posts_likes.user_id) -- значение user_id таблицы profiles которое соответствует значению user_id в таблице posts_like
+	AS gender -- название столбца gender
+	FROM posts_likes
+	ORDER BY like_type DESC; -- из таблицы posts_like
