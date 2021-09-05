70b8aa1c8f06664791fe0f1aabdda5f90d74aa34
alexsks65536 <megbyte@mail.ru>
2021-09-05 20:59:22 +0300

    Урок 7. Видеоурок. Сложные запросы

---
+++ lesson7/lesson7.sql
@@ -0,0 +1,54 @@
+/*
+ * 1. Составьте список пользователей users, 
+ * которые осуществили хотя бы один заказ orders в интернет магазине.
+ */
+
+SELECT id, name FROM users WHERE id IN (SELECT user_id FROM orders);
+
+/*
+ * 2. Выведите список товаров products и разделов catalogs, 
+ * который соответствует товару.
+ */
+
+SELECT id, name, (SELECT name FROM catalogs WHERE id = catalog_id) AS 'catalog' FROM products;
+
+/*
+*(по желанию) Пусть имеется таблица рейсов flights (id, from, to) 
+*и таблица городов cities (label, name). Поля from, to и label 
+*содержат английские названия городов, поле name — русское. 
+*Выведите список рейсов flights с русскими названиями городов.
+*/
+CREATE DATABASE example;
+
+CREATE TABLE flights(
+id SERIAL PRIMARY KEY,
+fly_from varchar(255) comment 'город вылета',
+fly_to varchar(255) comment 'город прилета'
+);
+-- truncate TABLE flights;
+INSERT INTO flights (fly_from, fly_to) VALUES 
+	('St.Peterburg', 'Moscow'),
+	('Moscow', 'Vladivostok'),
+	('Krasnodar', 'Anapa'),
+	('Moscow', 'Krasnodar');
+
+-- DROP TABLE cities;
+CREATE TABLE cities(
+lable varchar(255) comment 'города по-английски',
+name varchar(255) comment 'города по-русски'
+);
+
+INSERT INTO cities (lable, name) VALUES 
+	('St.Peterburg', 'Санкт-Петербург'),
+	('Moscow', 'Москва'),
+	('Vladivostok', 'Владивосток'),
+	('Krasnodar', 'Краснодар'),
+	('Anapa', 'Анапа');
+
+SELECT 
+	id, 
+	(SELECT name FROM cities WHERE lable = fly_from), 
+	(SELECT name FROM cities WHERE lable = fly_to) 
+	FROM flights 
+;
+
---
+++ lesson7/shop.sql
@@ -0,0 +1,125 @@
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+DROP TABLE IF EXISTS rubrics;
+CREATE TABLE rubrics (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела'
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO rubrics VALUES
+  (NULL, 'Видеокарты'),
+  (NULL, 'Память');
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  desсription TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, desсription, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+) COMMENT = 'Заказы';
+
+DROP TABLE IF EXISTS orders_products;
+CREATE TABLE orders_products (
+  id SERIAL PRIMARY KEY,
+  order_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Состав заказа';
+
+DROP TABLE IF EXISTS discounts;
+CREATE TABLE discounts (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
+  started_at DATETIME,
+  finished_at DATETIME,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id),
+  KEY index_of_product_id(product_id)
+) COMMENT = 'Скидки';
+
+DROP TABLE IF EXISTS storehouses;
+CREATE TABLE storehouses (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Склады';
+
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+  storehouse_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+insert into orders (id, user_id, created_at, updated_at) values (1, 3, '2020-12-01 03:54:15', '2020-12-24 01:37:11');
+insert into orders (id, user_id, created_at, updated_at) values (2, 2, '2021-06-11 06:04:50', '2021-03-17 00:36:24');
+insert into orders (id, user_id, created_at, updated_at) values (3, 2, '2020-08-28 02:37:55', '2021-01-12 16:30:50');
+insert into orders (id, user_id, created_at, updated_at) values (4, 1, '2020-10-01 07:49:35', '2020-09-19 05:38:39');
+insert into orders (id, user_id, created_at, updated_at) values (5, 1, '2021-03-14 05:14:18', '2021-05-11 22:26:27');
+insert into orders (id, user_id, created_at, updated_at) values (6, 3, '2021-07-08 13:19:19', '2021-05-13 05:50:15');
+insert into orders (id, user_id, created_at, updated_at) values (7, 3, '2021-01-05 12:15:54', '2020-09-30 02:44:45');
+insert into orders (id, user_id, created_at, updated_at) values (8, 2, '2021-02-18 08:30:18', '2021-06-02 10:10:38');
+insert into orders (id, user_id, created_at, updated_at) values (9, 3, '2021-06-15 02:51:34', '2020-09-19 06:48:33');
+insert into orders (id, user_id, created_at, updated_at) values (10, 1, '2020-10-28 15:41:18', '2021-07-16 22:05:09');
