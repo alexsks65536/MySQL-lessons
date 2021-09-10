2d8346005357fff3d903b60a4b1267271c6f6d18
alexsks65536 <megbyte@mail.ru>
2021-09-10 13:55:01 +0300

    Урок 8. “Операторы, фильтрация, сортировка и ограничение. Агрегация данных”

--- lesson6/lesson6.sql
+++ lesson6/lesson6.sql
@@ -12,6 +12,15 @@ GROUP BY from_user_id -- группируем
 ORDER BY total DESC -- сортируем по найденному кол-ву 'total'
 LIMIT 1; -- выводим только одно значение
 
+/*с использованием JOIN*/
+SELECT m.from_user_id, m.to_user_id, concat(first_name,' ', last_name) AS name_from_user, count(*) AS total
+FROM messages m 
+	JOIN users u ON u.id = m.from_user_id
+WHERE m.to_user_id = 6 
+GROUP BY m.from_user_id -- группируем
+ORDER BY total DESC -- сортируем по найденному кол-ву 'total'
+LIMIT 1; -- выводим только одно значение
+	
 /*
  * 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
  */
@@ -25,10 +34,32 @@ SELECT COUNT(*) as quantity_over_10 FROM profiles WHERE (YEAR(current_date())-YE
 SELECT user_id AS user_10 FROM profiles WHERE (YEAR(current_date())-YEAR(birthday)) < '10';
 -- 
 SELECT user_id AS user_10, (SELECT like_type FROM posts_likes WHERE profiles.user_id = user_10 ) FROM profiles WHERE (YEAR(current_date())-YEAR(birthday)) < '10';
-
 -- 
 SELECT user_id AS s, (YEAR(current_date())-YEAR(birthday)) AS age, (SELECT like_type FROM posts_likes WHERE user_id = s) FROM profiles ORDER BY age;
+/* с использованием JOIN */
+SELECT p.user_id, pl.like_type 
+FROM profiles p
+JOIN posts_likes pl ON pl.like_type = 1
+WHERE (YEAR(current_date())-YEAR(p.birthday)) < '10';
 
+SELECT count(*) AS total, pl.like_type
+FROM posts_likes pl
+JOIN profiles p ON pl.like_type = 1 AND (YEAR(current_date())-YEAR(p.birthday)) < '10';
+
+SELECT count(*) AS total, pl.like_type
+FROM posts_likes pl
+JOIN profiles p ON pl.like_type = 1
+WHERE (YEAR(current_date())-YEAR(p.birthday)) < '10';
+
+SELECT count(*) AS total
+FROM posts_likes pl
+JOIN profiles p ON (YEAR(current_date())-YEAR(p.birthday)) < '10'
+WHERE pl.like_type = 1;
+
+SELECT count(*) 
+FROM profiles p
+JOIN posts_likes pl ON (YEAR(now())-YEAR(p.birthday)) < '10'
+WHERE pl.like_type = 1;
 /*
  *3. Определить кто больше поставил лайков (всего): мужчины или женщины.
  */
---
+++ lesson8/lesson8.sql
@@ -0,0 +1,34 @@
+/* 
+ * Пусть задан некоторый пользователь. Из всех пользователей соц. сети найдите человека, 
+ * который больше всех общался с выбранным пользователем (написал ему сообщений).
+ */
+
+/*с использованием JOIN*/
+SELECT m.from_user_id, m.to_user_id, concat(first_name,' ', last_name) AS name_from_user, count(*) AS total
+FROM messages m 
+	JOIN users u ON u.id = m.from_user_id
+WHERE m.to_user_id = 6 
+GROUP BY m.from_user_id -- группируем
+ORDER BY total DESC -- сортируем по найденному кол-ву 'total'
+LIMIT 1; -- выводим только одно значение
+	
+/*
+ * 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
+ */
+SELECT count(*) AS users_under_10_years_old
+FROM profiles p
+JOIN posts_likes pl ON like_type = 1 AND p.user_id = pl.user_id 
+WHERE (YEAR(current_date())-YEAR(p.birthday)) < '10';
+
+/*
+ *3. Определить кто больше поставил лайков (всего): мужчины или женщины.
+ */
+SELECT count(*) AS female, (
+		SELECT count(*)
+		FROM posts_likes pl 
+		JOIN profiles p ON pl.user_id = p.user_id AND p.gender = 'm'
+		WHERE pl.like_type = 1
+		)  AS man
+	FROM posts_likes pl
+	JOIN profiles p ON pl.user_id = p.user_id AND p.gender = 'f'
+	WHERE pl.like_type = 1 ;
